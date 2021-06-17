e1f9164fc4083c08abd3b069ec71a5736a08de7f
ercprk <kyoungdukp@gmail.com>
2021-06-16 22:24:37 -0400

    Completed notuber lab part 1

--- notuber/README.md
+++ notuber/README.md
@@ -1,7 +1,7 @@
-# messages
+# notuber
 
 COMP 120: Web Programming and Engineering  
-Lab: Messages, Part 1
+Lab: The Ride-Hailing Service, Part 1 on Week 3
 
 ## Author
 
@@ -17,4 +17,11 @@ I have not collaborated or discussed the lab with anyone.
 
 ## Hours
 
-I spent about an hour completing this lab.
+I spent about three hours completing this lab.
+
+## Impact of Performance Enhancements
+
+No Optimization 
+2.5 kB transferred | Load time: 501 ms
+After performance optimizations
+2.4 kB transferred | Load time: 422 ms
--- notuber/index.js
+++ notuber/index.js
@@ -1,10 +1,34 @@
 /* Author: Eric Park */
 
 let map;
+var vehicles = new Array();
+var markers = new Array();
+
+vehicles.push({"id": "mXfkjrFw", "latitude": 42.3453, "longitude": -71.0464});
+vehicles.push({"id": "nZXB8ZHz", "latitude": 42.3662, "longitude": -71.0621});
+vehicles.push({"id": "Tkwu74WC", "latitude": 42.3603, "longitude": -71.0547});
+vehicles.push({"id": "5KWpnAJN", "latitude": 42.3472, "longitude": -71.0802});
+vehicles.push({"id": "uf5ZrXYw", "latitude": 42.3663, "longitude": -71.0544});
+vehicles.push({"id": "VMerzMH8", "latitude": 42.3542, "longitude": -71.0704});
 
 function initMap() {
+    var carIcon = {
+        url: "./images/car.png",
+        scale: 0.5
+    };
+
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat:  42.352271, lng: -71.05524200000001 },
-        zoom: 15
+        center: { lat: 42.352271, lng: -71.05524200000001 },
+        zoom: 14
+    });
+
+    vehicles.forEach((element, index, array) => {
+        var marker = new google.maps.Marker({
+            position: { lat: element["latitude"], lng: element["longitude"] },
+            icon: carIcon,
+            map: map
+        });
+
+        markers.push(marker);
     });
 }
